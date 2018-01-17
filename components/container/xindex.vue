20e79af2f5c79f02dd8f10b4843875b076e3c1d3
xiaotang66 <862115203@qq.com>
2018-01-17 15:17:32 +0800

    登录注册容器组件

--- components/container/xindex.vue
+++ components/container/xindex.vue
@@ -12,12 +12,23 @@
  import xbanner from "../index/xbanner.vue";
  import xadver from "../index/xadver.vue";
  import xtitle from "../index/xtitle.vue";
+ import $ from "jquery";
 export default{
     components:{
         xheader,
         xbanner,
         xadver,
         xtitle
+    },
+    data(){
+      return{
+
+      }
+    },
+    methods:{
+      collect(){
+        console.log($(this))
+      }
     }
 }
 </script>
---
+++ components/container/xregest.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="regest">
+	  <rheader/>
+	<form>
+	  <router-view></router-view>
+	  <rfooter/>
+	</form>
+ </div>
+</template>
+<style>
+html {height:100%}
+body{background-image:url("../../image/back.jpg");background-repeat:no-repeat;background-size:cover}
+ #regest{width:100%;height:100%;}
+ #regest form{font-size:0.28rem;margin:0 0.7rem }
+</style>
+<script>
+ import rheader from "../login/rheader.vue";
+ import  rfooter from "../login/rfooter.vue";
+export default{
+    components:{
+        rfooter,
+        rheader
+    }
+}
+</script>
